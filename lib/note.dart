d5e298c5ab0524ec4bfe421dbef38f5e0e9492a8
konstantin820820-cyber <konstantin820820@gmail.com>
2026-08-19 19:41:11 +0800

    note.dart

--- lib/note.dart
+++ lib/note.dart
@@ -1,18 +1,18 @@
 class Note {
   String id;
   String title;
-  String contentDelta;
+  String content;
   String category;
 
-  Note({required this.id, required this.title, required this.contentDelta, required this.category});
+  Note({required this.id, required this.title, required this.content, required this.category});
 
-  Map<String, dynamic> toMap() => {'id': id, 'title': title, 'contentDelta': contentDelta, 'category': category};
+  Map<String, dynamic> toMap() => {'id': id, 'title': title, 'content': content, 'category': category};
 
   factory Note.fromMap(Map<String, dynamic> map) {
     return Note(
       id: map['id'] ?? DateTime.now().millisecondsSinceEpoch.toString(),
       title: map['title'] ?? '',
-      contentDelta: map['contentDelta'] ?? '[{"insert":"\\n"}]',
+      content: map['content'] ?? '',
       category: map['category'] ?? 'Личное',
     );
   }
